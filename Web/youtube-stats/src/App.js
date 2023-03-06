b2b0fe86e3722924ce20b5f08b3d8108ded3c4f3
Nicolas de la Guardia <52568848+delaguardianick@users.noreply.github.com>
2023-03-05 19:16:11 -0500

    fix console log warning

--- Web/youtube-stats/src/App.js
+++ Web/youtube-stats/src/App.js
@@ -19,26 +19,26 @@ function App() {
 
   return (
   
-  <div class="root">
+  <div className="root">
 
     <body>
     
     {/* <!-- ======= Header ======= --> */}
-    <header id="header" class="header fixed-top d-flex align-items-center">
-      <div class="container d-flex align-items-center justify-content-between">
+    <header id="header" className="header fixed-top d-flex align-items-center">
+      <div className="container d-flex align-items-center justify-content-between">
 
-        <nav id="navbar" class="navbar">
+        <nav id="navbar" className="navbar">
           <ul>
           <div id="logo">
             <h1><a href=""><span>Youtube</span>Stats </a></h1>
             {/* <!-- Uncomment below if you prefer to use an image logo --> */}
             {/* <!-- <a href="index.html"><img src="assets/img/logo.png" alt="" title="" /></a>--> */}
           </div>
-            <li><a class="nav-link scrollto active" href="">Home</a></li>
-            <li><a class="nav-link scrollto" href="#help">Help</a></li>
-            <li><a class="nav-link scrollto" href="#contact">Github</a></li>
+            <li><a className="nav-link scrollto active" href="">Home</a></li>
+            <li><a className="nav-link scrollto" href="#help">Help</a></li>
+            <li><a className="nav-link scrollto" href="#contact">Github</a></li>
           </ul>
-          <i class="bi bi-list mobile-nav-toggle"></i>
+          <i className="bi bi-list mobile-nav-toggle"></i>
         </nav>
 
       </div>
@@ -47,56 +47,56 @@ function App() {
 
 
     <main id="main">
-      <div class="main-container">
+      <div className="main-container">
 
         {/* <!-- ======= Hero Section ======= --> */}
         <section id="hero">
-          <div class="hero-container" data-aos="fade-in">
+          <div className="hero-container" data-aos="fade-in">
             <h1>Watch History Analysis</h1>
             <h2>Analyze your youtube watch trends</h2>
             {/* <img src="assets/img/hero-img.png" alt="Hero Imgs" data-aos="zoom-out" data-aos-delay="100"> */}
-            <a href="#get-started" class="btn-get-started scrollto">Upload Takeout</a>
+            <a href="#get-started" className="btn-get-started scrollto">Upload Takeout</a>
           </div>
         </section>
 
         {/* <!-- ======= Plots Section ======= --> */}
         <section id="plots">
-          <div class="plots-container">
-            <div class="div1 plot-image">
+          <div className="plots-container">
+            <div className="div1 plot-image">
               <span>Plot1</span>
               {/* <img src="https://www.amcharts.com/wp-content/uploads/2019/10/demo_14593_none-7.png"></img> */}
               {console.log(plotUrl)}
               {plotUrl && <img src={plotUrl} alt="Watch time by weekday" />}
             </div>
-            <div class="div2 plot-image">
+            <div className="div2 plot-image">
               <span>Plot2</span>
               <img src="https://www.bankrate.com/2022/04/19101010/Tesla-stock-chart.jpg"></img>
             </div>
-            <div class="div3 plot-image">
+            <div className="div3 plot-image">
               <span>Plot3</span>
               <img src="https://www.amcharts.com/wp-content/uploads/2019/10/demo_14593_none-7.png"></img>
             </div>
-            <div class="div4 plot-image">
+            <div className="div4 plot-image">
               <span>Plot4</span>
               <img src="https://www.amcharts.com/wp-content/uploads/2019/10/demo_14593_none-7.png"></img>
             </div>
-            <div class="div5 plot-image">
+            <div className="div5 plot-image">
               <span>Plot5</span>
               <img src="https://www.amcharts.com/wp-content/uploads/2019/10/demo_14593_none-7.png"></img>
             </div>
-            <div class="div6 plot-image">
+            <div className="div6 plot-image">
               <span>Plot6</span>
               <img src="https://www.amcharts.com/wp-content/uploads/2019/10/demo_14593_none-7.png"></img>
             </div>
-            <div class="div7 plot-image">
+            <div className="div7 plot-image">
               <span>Plot7</span>
               <img src="https://www.amcharts.com/wp-content/uploads/2019/10/demo_14593_none-7.png"></img>
             </div>
-            <div class="div8 plot-image">
+            <div className="div8 plot-image">
               <span>Plot8</span>
               <img src="https://www.amcharts.com/wp-content/uploads/2019/10/demo_14593_none-7.png"></img>
             </div>
-            <div class="div9 plot-image">
+            <div className="div9 plot-image">
               <span>Plot9</span>
               <img src="https://www.amcharts.com/wp-content/uploads/2019/10/demo_14593_none-7.png"></img>
             </div>
@@ -106,16 +106,16 @@ function App() {
     </main>
 
     {/* <!-- ======= Footer ======= --> */}
-    <footer class="footer">
-      <div class="container">
-        <div class="row">
+    <footer className="footer">
+      <div className="container">
+        <div className="row">
 
-          <div class="col-sm-6 col-md-3 col-lg-2">
-            <div class="list-menu">
+          <div className="col-sm-6 col-md-3 col-lg-2">
+            <div className="list-menu">
 
               <h4>About Us</h4>
 
-              <ul class="list-unstyled">
+              <ul className="list-unstyled">
                 <li><a href="#">About us</a></li>
                 <li><a href="#">Features item</a></li>
                 <li><a href="#">Live streaming</a></li>
@@ -130,7 +130,7 @@ function App() {
 
     </footer>
 
-    <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+    <a href="#" className="back-to-top d-flex align-items-center justify-content-center"><i className="bi bi-arrow-up-short"></i></a>
 
     {/* <!-- Vendor JS Files --> */}
     <script src="assets/vendor/aos/aos.js"></script>
