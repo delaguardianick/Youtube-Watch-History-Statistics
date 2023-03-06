f90a2dd35cf1f6564ea3d301209bae9156e513ab
Nicolas de la Guardia <52568848+delaguardianick@users.noreply.github.com>
2023-03-05 19:33:53 -0500

    format and refactoring

--- Backend/src/Youtube_Analysis_Service.py
+++ Backend/src/Youtube_Analysis_Service.py
@@ -11,56 +11,96 @@ from matplotlib.figure import Figure
 import base64
 from io import BytesIO
 
-class Plots:
-    def watch_time_weekday():
 
+class Plots:
+    last_video_date = None
+    first_video_date = None
+
+    # Get weeks diff betweendate first and last video in df
+    def calculate_time_difference(wh_df):
+        last_video_date = parser.parse(wh_df["date_time_iso"].iloc[0])
+        first_video_date = parser.parse(wh_df["date_time_iso"].iloc[-1])
+        difference = last_video_date - first_video_date
+        date_ranges = {
+            "years": difference.days // 365,
+            "weeks": difference.days // 7,
+            "days": difference.days,
+            "hours": difference.days * 24,
+            "minutes": difference.days * 60,
+        }
+
+        return date_ranges
+
+    def analyze_data(self):
         con = sqlite3.connect("../SQLite/YoutubeStats.sqlite")
-        watch_history_all_df = pd.read_sql_query(
-            "SELECT * from watch_history_v1", con)
+        watch_history_all_df = pd.read_sql_query("SELECT * from watch_history_v1", con)
         wh_df = watch_history_all_df[watch_history_all_df.is_available == 1]
 
         # Data for year range
         start_year = 2016
         end_year = 2021
         year_range = (start_year, end_year)
-        year_range_string = str(
-            year_range[0]) if year_range[0] == year_range[1] else f"{year_range[0]} - {year_range[1]}"
+        year_range_string = (
+            str(year_range[0])
+            if year_range[0] == year_range[1]
+            else f"{year_range[0]} - {year_range[1]}"
+        )
         label_videos_over_range = f"# of videos watched ({year_range_string})"
         # range(year_range[0], year_range[1]+1):
-        wh_df = wh_df[(year_range[0] <= wh_df.year_date) &
-                    (wh_df.year_date <= year_range[1])]
+        wh_df = wh_df[
+            (year_range[0] <= wh_df.year_date) & (wh_df.year_date <= year_range[1])
+        ]
         video_count_in_df = len(wh_df.watch_id)
 
-        # Get weeks diff betweendate first and last video in
-        last_video_date = parser.parse(wh_df["date_time_iso"].iloc[0])
-        first_video_date = parser.parse(wh_df["date_time_iso"].iloc[-1])
-        difference = last_video_date - first_video_date
-        years_diff_date_range = difference.days // 365
-        weeks_diff_date_range = difference.days // 7
-        days_diff_date_range = difference.days
-        hours_diff_date_range = difference.days * 24
-        minutes_diff_date_range = difference.days * 60
-        # VIDEO
-        # Change string to int
-        wh_df['video_length_secs'] = wh_df['video_length_secs'].astype(int)
+        # Calculate time range
+        date_ranges = self.calculate_time_difference(wh_df)
 
+        # Change string to int
+        wh_df["video_length_secs"] = wh_df["video_length_secs"].astype(int)
+
+        return wh_df, date_ranges
+
+    def getAllPlots(self):
+        wh_df, date_ranges = self.analyze_data(self)
+        plots = []
+        plots.append(Plots.watch_time_weekday(wh_df, date_ranges.get("weeks")))
+        plots.append(Plots.watch_time_hour())
+        plots.append(Plots.watch_time_year())
+        plots.append(Plots.watch_time_month())
+        plots.append(Plots.watch_time_day())
+        return plots
+
+    def watch_time_weekday(wh_df, weeks_diff_date_range):
         # Filter dataframe and group by desired index
         weekdays_count_df = wh_df[["video_length_secs", "day_of_week"]]
-        weekdays_count_df = weekdays_count_df.groupby(
-            "day_of_week").sum().reset_index()
+        weekdays_count_df = weekdays_count_df.groupby("day_of_week").sum().reset_index()
 
         # Data manipulation. total secs for a sunday into hours watched on average on a sunday
-        weekdays_count_df['hours_watched_avg'] = weekdays_count_df['video_length_secs'].apply(
-            lambda x: x / (60*60*weeks_diff_date_range))
+        weekdays_count_df["hours_watched_avg"] = weekdays_count_df[
+            "video_length_secs"
+        ].apply(lambda x: x / (60 * 60 * weeks_diff_date_range))
 
         # label mapping
-        weekdays_map = {0: 'Mon', 1: 'Tues', 2: 'Wed',
-                        3: 'Thurs', 4: 'Fri', 5: 'Sat', 6: 'Sun'}
+        weekdays_map = {
+            0: "Mon",
+            1: "Tues",
+            2: "Wed",
+            3: "Thurs",
+            4: "Fri",
+            5: "Sat",
+            6: "Sun",
+        }
         weekdays_count_df["day_of_week"] = weekdays_count_df["day_of_week"].map(
-            weekdays_map)
+            weekdays_map
+        )
 
-        return weekdays_count_df.plot(x="day_of_week", y="hours_watched_avg", title="Avg Watch Time / Weekday",
-                                    xlabel="Weekdays", ylabel="Hours watched on average").legend([f"Range: {start_year} - {end_year}"])
+        return weekdays_count_df.plot(
+            x="day_of_week",
+            y="hours_watched_avg",
+            title="Avg Watch Time / Weekday",
+            xlabel="Weekdays",
+            ylabel="Hours watched on average",
+        ).legend([f"Range: {start_year} - {end_year}"])
 
 
 # # In[53]:
--- Web/youtube-stats/src/App.js
+++ Web/youtube-stats/src/App.js
@@ -19,10 +19,8 @@ function App() {
 
   return (
   
-  <div className="root">
+  <div className="body">
 
-    <body>
-    
     {/* <!-- ======= Header ======= --> */}
     <header id="header" className="header fixed-top d-flex align-items-center">
       <div className="container d-flex align-items-center justify-content-between">
@@ -142,9 +140,7 @@ function App() {
     {/* <!-- Template Main JS File --> */}
     <script src="assets/js/main.js"></script>
 
-  </body>
-
-</div>
+  </div>
 
   );
 }
