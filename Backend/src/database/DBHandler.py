09d6600c41a5f4e5d84b734f9945478004f726d2
Nicolas de la Guardia <52568848+delaguardianick@users.noreply.github.com>
2023-03-28 21:04:57 -0400

    Added DbHandler for connections

--- Backend/src/database/DBHandler.py
+++ Backend/src/database/DBHandler.py
@@ -21,27 +21,12 @@ class DBHandler:
             print("Connecting to the PostgreSQL database...")
             conn = psycopg2.connect(**params)
 
-            # create a cursor
-            cur = conn.cursor()
+            return conn
 
-            # execute a statement
-            print("PostgreSQL database version:")
-            cur.execute("SELECT version()")
-
-            # display the PostgreSQL database server version
-            db_version = cur.fetchone()
-            print(db_version)
-
-            # close the communication with the PostgreSQL
-            cur.close()
         except (Exception, psycopg2.DatabaseError) as error:
             print(error)
-        finally:
-            if conn is not None:
-                conn.close()
-                print("Database connection closed.")
 
 
-if __name__ == "__main__":
-    db_handler = DBHandler()
-    db_handler.connect()
+# if __name__ == "__main__":
+#     db_handler = DBHandler()
+#     db_handler.connect()
