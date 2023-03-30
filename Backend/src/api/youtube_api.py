cf27f610e937452882970ed7effe76156a8566e1
Nicolas de la Guardia <52568848+delaguardianick@users.noreply.github.com>
2023-03-30 18:06:42 -0400

    Regenerated exposed API key

---
+++ Backend/src/api/youtube_api.py
@@ -0,0 +1,28 @@
+import json
+import requests
+import os
+import sys
+
+
+class YoutubeApi:
+    def api_get_video_details(self, video_ids_to_query) -> json:
+        api_key = YoutubeApi.get_api_key()
+        request_url = f"https://www.googleapis.com/youtube/v3/videos?part=snippet,contentDetails&id={video_ids_to_query}&key={api_key}"
+        video_details = requests.get(request_url).json()
+
+        return video_details
+
+    def get_api_key():
+        # Construct the path to the API key file
+        script_dir = os.path.dirname(os.path.abspath(__file__))
+        api_key_file = "youtube_api_key.txt"
+        file_path = script_dir + "\\" + api_key_file
+
+        # Read the API key from the file
+        try:
+            with open(file_path, "r") as f:
+                api_key = f.read().strip()
+                return api_key
+        except FileNotFoundError:
+            print("Error: youtube_api_key.txt not found.")
+            return None
